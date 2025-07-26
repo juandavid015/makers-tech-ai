332688192b5967b9c6a00d2ff6aab7243321830c
juandavid015 <juandavidgr1002@gmail.com>
2025-07-26 02:00:58 -0500

    feat: enhance layout and integrate chatbot component into the homepage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,10 @@
+import Chatbot from "@/features/chatbot/components/container";
 
 export default function Home() {
   return (
-    <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
+    <main className="flex flex-col items-center justify-center w-full min-h-screen font-sans">
       <h1>Hello World</h1>
+      <Chatbot />
     </main>
   );
 }
