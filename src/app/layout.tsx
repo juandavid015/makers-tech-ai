bf10f2db9c73a56d2f92a06bdf3b095ecbbc54f1
juandavid015 <juandavidgr1002@gmail.com>
2025-07-26 21:45:50 -0500

    feat: integrate Header and Footer components into RootLayout for improved site structure and navigation

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,8 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import Chatbot from "@/features/chatbot/components/container";
+import Header from "@/components/layout/header";
+import Footer from "@/components/layout/footer";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -28,8 +30,10 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased min-h-screen h-full w-full relative`}
       >
+        <Header />
         {children}
         <Chatbot />
+        <Footer />
       </body>
     </html>
   );
